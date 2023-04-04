ed070109678e8f594367cb9638eb9501c08e8293
Helen <elena.kozovyakina@gmail.com>
2023-04-04 12:58:22 +0300

    parsing

--- main.py
+++ main.py
@@ -6849,7 +6849,7 @@ import time
 import csv
 
 # with open('data.csv') as f:
-#     file_reader = csv.reader(f, delimiter=";")
+#     file_reader = csv.reader(f, delimiter=";")  # []
 #     count = 0
 #     for row in file_reader:
 #         if count == 0:
@@ -6860,13 +6860,390 @@ import csv
 #     print(f"Всего в файле {count} строки.")
 
 
-with open('data.csv') as f:
-    file_names = ['Имя', 'Профессия', 'Год рождения']
-    file_reader = csv.DictReader(f, delimiter=";", fieldnames=file_names)
-    count = 0
-    for row in file_reader:
-        # print(row)
-        if count == 0:
-            print(f"Файл содержит столбцы: {', '.join(row)}")
-        print(f"\t{row['Имя']} - {row['Профессия']}. Родился в {row['Год рождения']} году.")
-        count += 1
+# with open('data.csv') as f:
+#     file_names = ['Имя', 'Профессия', 'Год рождения']
+#     file_reader = csv.DictReader(f, delimiter=";", fieldnames=file_names)  # {}
+#     count = 0
+#     for row in file_reader:
+#         # print(row)
+#         if count == 0:
+#             print(f"Файл содержит столбцы: {', '.join(row)}")
+#         print(f"\t{row['Имя']} - {row['Профессия']}. Родился в {row['Год рождения']} году.")
+#         count += 1
+
+
+# with open('student.csv', 'w') as f:
+#     write = csv.writer(f, delimiter=';', lineterminator='\r')
+#     write.writerow(['Имя', 'Класс', 'Возраст'])
+#     write.writerow(['Женя', '9', '15'])
+#     write.writerow(['Саша', '5', '12'])
+#     write.writerow(['Маша', '11', '18'])
+
+
+# data = [['hostname', 'vendor', 'model', 'location'],
+#         ['sw1', 'Cisco', '3750', 'London, Best str'],
+#         ['sw2', 'Cisco', '3850', 'Liverpool, Better str'],
+#         ['sw3', 'Cisco', '3650', 'Liverpool, Better str'],
+#         ['sw4', 'Cisco', '3650', 'London, Best str']]
+#
+#
+# with open('sw_data.csv', 'w') as f:
+#     writer = csv.writer(f, delimiter=';', lineterminator='\r')
+#     # for row in data:
+#     #     writer.writerow(row)
+#     writer.writerows(data)
+#
+#
+# with open('sw_data.csv', 'r') as f:
+#     print(f.read())
+
+# with open('stud_1.csv', 'w') as f:
+#     name = ["Имя", "Возраст"]
+#     file_writer = csv.DictWriter(f, delimiter=';', lineterminator='\r', fieldnames=name)
+#     file_writer.writeheader()
+#     file_writer.writerow({"Имя": "Саша", "Возраст": "6"})
+#     file_writer.writerow({"Имя": "Маша", "Возраст": "15"})
+#     file_writer.writerow({"Имя": "Вова", "Возраст": "14"})
+
+
+# data = [{
+#     'hostname': 'sw1',
+#     'location': 'London',
+#     'model': '3750',
+#     'vendor': 'Cisco'
+# }, {
+#     'hostname': 'sw2',
+#     'location': 'Liverpool',
+#     'model': '3850',
+#     'vendor': 'Cisco'
+# }, {
+#     'hostname': 'sw3',
+#     'location': 'Liverpool',
+#     'model': '3650',
+#     'vendor': 'Cisco'
+# }, {
+#     'hostname': 'sw4',
+#     'location': 'London',
+#     'model': '3650',
+#     'vendor': 'Cisco'
+# }]
+#
+# with open('dict_writer.csv', 'w') as f:
+#     writer = csv.DictWriter(f, delimiter=';', lineterminator='\r', fieldnames=list(data[0].keys()))
+#     writer.writeheader()
+#     for d in data:
+#         writer.writerow(d)
+
+
+# Парсинг данных с сайта
+
+# pip install beatifulsoup4 или bs4
+
+# from bs4 import BeautifulSoup
+
+# f = open('index.html').read()
+# soup = BeautifulSoup(f, 'html.parser')
+# # row = soup.find_all("div", class_="row")[2]
+# # row = soup.find("div", class_="name")
+# # row = soup.find_all("div", class_="row")[1].find("div", class_="links")
+# # row = soup.find_all("div", class_="links")[1]
+# # row = soup.find_all("div", {"data-set": "salary"})
+# # row = soup.find("div", string="Alena").parent.parent
+# # row = soup.find("div", string="Alena").find_parent(class_="row")
+# # row = soup.find("div", id="whois3").find_next_sibling()
+# row = soup.find("div", id="whois3").find_previous_sibling()
+# print(row)
+
+# def get_copywriter(tag):
+#     whois = tag.find('div', class_="whois")
+#     if "Copywriter" in whois:
+#         return tag
+#     return None
+#
+#
+# f = open('index.html', encoding="utf-8").read()
+# soup = BeautifulSoup(f, 'html.parser')
+# copywriter = []
+# row = soup.find_all("div", class_='row')
+# for i in row:
+#     cw = get_copywriter(i)
+#     if cw:
+#         copywriter.append(cw)
+# print(copywriter)
+# import re
+#
+#
+# def get_salary(s):
+#     pattern = r'\d+'
+#     # res = re.findall(pattern, s)[0]
+#     res = re.search(pattern, s).group()
+#     print(res)
+#
+#
+# f = open('index.html', encoding="utf-8").read()
+# soup = BeautifulSoup(f, 'html.parser')
+# row = soup.find_all("div", {"data-set": "salary"})
+# for i in row:
+#     get_salary(i.text)
+
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+# # res = requests.get('https://ru.wordpress.org/')
+# # # print(res.status_code)
+# # # print(res.headers['content-type'])
+# # # print(res.content)
+# # print(res.text)
+# # print(type(res.text))
+#
+# def get_html(url):
+#     res = requests.get(url)
+#     return res.text
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, "lxml")  # pip install lxml
+#     p1 = soup.find("header", id="masthead").find("p", class_="site-title").text
+#     return p1
+#
+#
+# def main():
+#     url = 'https://ru.wordpress.org/'
+#     print(get_data(get_html(url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
+# import requests
+# from bs4 import BeautifulSoup
+# import re
+# import csv
+#
+#
+# def get_html(url):
+#     res = requests.get(url)
+#     return res.text
+#
+#
+# def refined(s):
+#     return re.sub(r"\D+", "", s)
+#
+#
+# def write_csv(data):
+#     with open('plugins.csv', 'a') as f:
+#         writer = csv.writer(f, delimiter=';', lineterminator='\r')
+#
+#         writer.writerow((data['name'], data['url'], data['rating']))
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, "lxml")
+#     p1 = soup.find_all('section', class_="plugin-section")[1]
+#     plugins = p1.find_all('article')
+#
+#     for plugin in plugins:
+#         name = plugin.find('h3').text
+#         url = plugin.find('h3').find("a").get('href')
+#         # url = plugin.find('h3').find("a")['href']
+#         rating = plugin.find("span", class_="rating-count").find('a').text
+#         r = refined(rating)
+#         data = {'name': name, "url": url, "rating": r}
+#         write_csv(data)
+#
+#
+# def main():
+#     url = 'https://ru.wordpress.org/plugins/'
+#     get_data(get_html(url))
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+# import json
+# # data = {}
+#
+#
+# class CountryCapital:
+#     @staticmethod
+#     def load(file_name):
+#         try:
+#             data = json.load(open(file_name))
+#         except FileNotFoundError:
+#             data = {}
+#         finally:
+#             return data
+#
+#     @staticmethod
+#     def add_country(file_name):
+#         new_country = input("Введите название страны: ")
+#         new_capital = input("Введите название столицы: ")
+#         # try:
+#         #     data1 = json.load(open(file_name))
+#         # except FileNotFoundError:
+#         #     data1 = {}
+#         data1 = CountryCapital.load(file_name)
+#
+#         data1[new_country] = new_capital
+#
+#         with open(file_name, 'w') as f:
+#             json.dump(data1, f, indent=2)
+#
+#     @staticmethod
+#     def delete_country(file_name):
+#         del_country = input('Введите название страны: ')
+#         # try:
+#         #     data1 = json.load(open(file_name))
+#         # except FileNotFoundError:
+#         #     data1 = {}
+#         data1 = CountryCapital.load(file_name)
+#
+#         if del_country in data1:
+#             del data1[del_country]
+#
+#             with open(file_name, 'w') as f:
+#                 json.dump(data1, f, indent=2)
+#         else:
+#             print("Такой страны в базе нет")
+#
+#     @staticmethod
+#     def search_data(file_name):
+#         # try:
+#         #     data1 = json.load(open(file_name))
+#         # except FileNotFoundError:
+#         #     data1 = {}
+#         data1 = CountryCapital.load(file_name)
+#         country = input("Введите название страны: ")
+#         if country in data1:
+#             print(f"Страна {country}, столица {data1[country]} есть в словаре")
+#         else:
+#             print(f"Страны {country} нет в словаре")
+#
+#     @staticmethod
+#     def edit_data(file_name):
+#         country = input("Введите страну для корректировки: ")
+#         new_capital = input("Введите новое название столицы: ")
+#         # try:
+#         #     data1 = json.load(open(file_name))
+#         # except FileNotFoundError:
+#         #     data1 = {}
+#         data1 = CountryCapital.load(file_name)
+#
+#         if country in data1:
+#             data1[country] = new_capital
+#
+#             with open(file_name, 'w') as f:
+#                 json.dump(data1, f, indent=2)
+#         else:
+#             print("Такой страны в базе нет")
+#
+#     @staticmethod
+#     def load_from_file(file_name):
+#         with open(file_name) as f:
+#             print(json.load(f))
+#
+#
+# file = "list_capital.json"
+# while True:
+#     index = input('Выбор действия:\n1 - добавление данных\n2 - удаление данных\n'
+#                   '3 - поиск данных\n4 - редактирование данных\n5 - просмотр данных\n'
+#                   '6 - завершение работы\nВвод: ')
+#     if index == "1":
+#         CountryCapital.add_country(file)
+#     elif index == "2":
+#         CountryCapital.delete_country(file)
+#     elif index == "3":
+#         CountryCapital.search_data(file)
+#     elif index == "4":
+#         CountryCapital.edit_data(file)
+#     elif index == "5":
+#         CountryCapital.load_from_file(file)
+#     elif index == "6":
+#         break
+#     else:
+#         print("Введен некорректный номер")
+
+
+# import requests
+# from bs4 import BeautifulSoup
+# import csv
+#
+#
+# def get_html(url):
+#     res = requests.get(url)
+#     return res.text
+#
+#
+# def refine_cy(s):
+#     return s.split()[-1]
+#
+#
+# def write_csv(data):
+#     with open('plugins1.csv', 'a', encoding='utf-8-sig') as f:
+#         writer = csv.writer(f, delimiter=';', lineterminator='\r')
+#         writer.writerow((data['name'], data['url'], data['snippet'], data['active_install'], data['tests']))
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, "lxml")
+#     element = soup.find_all('article', class_='plugin-card')
+#     for el in element:
+#         try:
+#             name = el.find('h3').text
+#         except ValueError:
+#             name = ''
+#
+#         try:
+#             url = el.find('h3').find('a').get('href')
+#         except ValueError:
+#             url = ''
+#
+#         try:
+#             snippet = el.find('div', class_="entry-excerpt").text.strip()
+#         except ValueError:
+#             snippet = ''
+#
+#         try:
+#             active = el.find('span', class_="active-installs").text.strip()
+#         except ValueError:
+#             active = ''
+#
+#         try:
+#             c = el.find('span', class_="tested-with").text.strip()
+#             cy = refine_cy(c)
+#         except ValueError:
+#             cy = ''
+#
+#         data = {
+#             'name': name,
+#             'url': url,
+#             'snippet': snippet,
+#             'active_install': active,
+#             'tests': cy
+#         }
+#
+#         write_csv(data)
+#
+#
+# def main():
+#     for i in range(9, 26):
+#         url = f'https://ru.wordpress.org/plugins/browse/blocks/page/{i}/'
+#         get_data(get_html(url))
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
+from parse import Parser
+
+
+def main():
+    pars = Parser("https://www.ixbt.com/live/index/news/", "news.txt")
+    pars.run()
+
+
+if __name__ == '__main__':
+    main()
