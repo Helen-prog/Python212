eab685770f20a10c4f3ad69da197ff555e5309ef
Helen <elena.kozovyakina@gmail.com>
2023-04-06 13:01:31 +0300

    mvc

--- main.py
+++ main.py
@@ -7237,13 +7237,34 @@ import csv
 #     main()
 
 
-from parse import Parser
+# from parse import Parser
+#
+#
+# def main():
+#     pars = Parser("https://www.ixbt.com/live/index/news/", "news.txt")
+#     pars.run()
+#
+#
+# if __name__ == '__main__':
+#     main()
 
 
-def main():
-    pars = Parser("https://www.ixbt.com/live/index/news/", "news.txt")
-    pars.run()
+import socket
+
+
+def run():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 5000))
+    server_socket.listen()
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        request = client_socket.recv(1024)
+
+        print(f"Клиент: {addr} => \n{request}\n")
 
 
 if __name__ == '__main__':
-    main()
+    run()
+
+
